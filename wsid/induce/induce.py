2d0e3339cd2ee108ee3f905bc08f4d064bbcdf8e
Artem Revenko <artreven@gmail.com>
2019-02-08 16:08:08 +0100

    Removing drawing functions form package

--- wsid/induce/induce.py
+++ wsid/induce/induce.py
@@ -3,7 +3,7 @@ import logging
 import math
 import time
 
-import networkx as nx
+# import networkx as nx
 import numpy as np
 from functools import reduce
 
@@ -11,7 +11,6 @@ from functools import reduce
 import igraph as ig
 import scipy.sparse
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.preprocessing import MinMaxScaler
 
 import wsid.cooccurrence as cooc
 
@@ -106,92 +105,6 @@ def induce(cos, token2ind, ind2token, entity_str,
 #         if c >= limit_per_cluster:
 #             break
 #     return nodes_colors, nodes_weights
-
-
-def make_plotly_fig(G, node_weights, nodes_colors, fig_title='',
-                    show_fig=False):
-    import plotly.offline as po
-    import plotly.graph_objs as go
-
-    pos = nx.drawing.nx_agraph.graphviz_layout(G)
-    edges = list(G.edges(data='weight'))
-    # scale width
-    edges_width = MinMaxScaler(feature_range=(0, 7)).fit_transform(
-        np.array([x[2] for x in edges]).reshape(-1, 1)
-    )
-
-    edge_trace = go.Scatter(
-        x=[],
-        y=[],
-        mode='lines'
-    )
-    for i in range(len(edges)):
-        x0, y0 = pos[edges[i][0]]
-        x1, y1 = pos[edges[i][1]]
-        width = edges_width[i][0]
-
-        edge_trace['x'] += (x0, x1, None)
-        edge_trace['y'] += (y0, y1, None)
-        edge_trace['hoverinfo'] = 'none'
-        edge_trace['line']['width'] = width
-        edge_trace['line']['color'] = '#888'
-
-
-    node_trace = go.Scatter(
-        x=[],
-        y=[],
-        text=[],
-        mode='markers',
-        hoverinfo='text',
-        marker=go.scatter.Marker(
-            color=[],
-            size=[],
-            line=dict(width=2)
-        )
-    )
-
-    annotations = []
-    max_weight = max(node_weights.values())
-    for node in G.nodes():
-        x, y = pos[node]
-        node_info = '{}, weight: {:.2e}'.format(
-            str(node),
-            node_weights[node]
-        )
-        scaled_size = 40 * node_weights[node] / max_weight
-
-        node_trace['x'] += (x,)
-        node_trace['y'] += (y,)
-        node_trace['marker']['color'] += (nodes_colors[node],)
-        node_trace['marker']['size'] += (scaled_size,)
-        node_trace['text'] += (node_info,)
-        ann_dict = {
-            'x': x, 'y': y, 'text': str(node), 'showarrow': False
-        }
-        annotations.append(ann_dict)
-
-    fig = go.Figure(data=go.Data([edge_trace, node_trace]),
-                    layout=go.Layout(
-                        title=fig_title,
-                        titlefont=dict(size=16),
-                        showlegend=False,
-                        width=900,
-                        height=650,
-                        hovermode='closest',
-                        margin=dict(b=20, l=5, r=5, t=40),
-                        annotations=annotations,
-                        xaxis=go.layout.XAxis(showgrid=False, zeroline=False,
-                                              showticklabels=False),
-                        yaxis=go.layout.YAxis(showgrid=False, zeroline=False,
-                                              showticklabels=False))
-                    )
-    if not show_fig:
-        output = 'div'
-    else:
-        output = 'file'
-    return po.plot(fig, filename='networkx.html',
-                   auto_open=show_fig,
-                   output_type=output)
 
 
 def compare_senses(senses):
