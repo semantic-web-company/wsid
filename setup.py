72555a7c07ec3cb27ad33355728cb81946887e88
Artem Revenko <artreven@gmail.com>
2019-06-27 12:18:37 +0200

    Change setup to using pbr

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup
 
 setup(
-    name='wsid',
-    version='0.0.3',
-    packages=['wsid', 'tests'],
-    url='',
-    install_requires=[
-        'nltk>=3.0.5',
-        'numpy>=1.9.3',
-        'scipy>=0.13.3',
-        'scikit-learn>=0.17',
-        'matplotlib>=1.4.3',
-        'diskcache',
-        'python-igraph'
-    ],
-    author='Artem Revenko (artreven)',
-    author_email='artreven@gmail.com',
-    description='Induction and disambiguation of word senses using cooccurent words'
+    setup_requires=['pbr'],
+    pbr=True,
 )
